56ef38118a48ace46d13648d9d89373de6602644
Tanveer Alam <ansaritaz78601@gmail.com>
2026-02-21 20:22:17 +0530

    added the features

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
-import React from "react";
-import { fetchGIF, fetchPhotos, fetchVideo } from "./api/mediaApi";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
     <div className="h-screen w-full bg-gray-950 text-white">
-      <button
-        className="bg-green-400 px-4 py-2 m-5 rounded"
-        onClick={async () => {
-          const data = await fetchPhotos("cat");
+      <SearchBar/>
 
-          console.log(data);
-        }}
-      >
-        Get Photos
-      </button>
-
-      <button
-        className="bg-green-400 px-4 py-2 m-5 rounded"
-        onClick={async () => {
-          const data = await fetchVideo("cat");
-          console.log(data.videos);
-        }}
-      >
-        Get Videos
-      </button>
-
-      <button
-        className="bg-green-400 px-4 py-2 m-5 rounded"
-        onClick={async () => {
-          const data = await fetchGIF("cat");
-          console.log(data.data);
-        }}
-      >
-        Get GIF
-      </button>
     </div>
   );
 };
