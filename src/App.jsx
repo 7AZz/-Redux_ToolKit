fc037a0bfdd65092808ab928c5d3e2b8a18e428b
Tanveer Alam <ansaritaz78601@gmail.com>
2026-02-21 10:17:47 +0530

    called The APIS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { fetchGIF, fetchPhotos, fetchVideo } from "./api/mediaApi";
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-950 text-white'>
-      
-    </div>
-  )
-}
+    <div className="h-screen w-full bg-gray-950 text-white">
+      <button
+        className="bg-green-400 px-4 py-2 m-5 rounded"
+        onClick={async () => {
+          const data = await fetchPhotos("cat");
 
-export default App
+          console.log(data);
+        }}
+      >
+        Get Photos
+      </button>
+
+      <button
+        className="bg-green-400 px-4 py-2 m-5 rounded"
+        onClick={async () => {
+          const data = await fetchVideo("cat");
+          console.log(data.videos);
+        }}
+      >
+        Get Videos
+      </button>
+
+      <button
+        className="bg-green-400 px-4 py-2 m-5 rounded"
+        onClick={async () => {
+          const data = await fetchGIF("cat");
+          console.log(data.data);
+        }}
+      >
+        Get GIF
+      </button>
+    </div>
+  );
+};
+
+export default App;
--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -1,6 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
-const UNSPLASH_KEY= import.meta.env.VITE_UNSPLASH_KEY
-const PEXELS_KEY= import.meta.env.VITE_PEXELS_KEY
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
+const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY;
 
+export async function fetchPhotos(query, page = 1, perPage = 20) {
+  const res = await axios.get("https://api.unsplash.com/photos", {
+    params: { query, page, perPage },
+    headers: { Authorization: `Client-ID ${UNSPLASH_KEY}` },
+  });
 
+  return res.data;
+}
+
+export async function fetchVideo(query, perPage = 20) {
+  const res = await axios.get("https://api.pexels.com/videos/search", {
+    params: { query, perPage },
+    headers: { Authorization: PEXELS_KEY },
+  });
+
+  return res.data;
+}
+
+export async function fetchGIF(query, limit = 20) {
+  const res = await axios.get("https://api.giphy.com/v1/gifs/search", {
+    params: { api_key: GIPHY_KEY, q: query, limit },
+  });
+  return res.data;
+}
